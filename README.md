ee609efadb6cb6f0c36f2003ca019e3bdc45c032
andreagonzalezperez <andreagonzalezperez@gmail.com>
2026-09-25 18:07:06 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# ritmoacade
-Public repository for ritmoacade
+# RitmoAcade 2.0
+Versión móvil/PWA avanzada y autocontenida.
+
+Módulos:
+- Hoy
+- Horario/calendario
+- Estudio + objetivo semanal + temporizador
+- Repertorio con progreso
+- Tareas y prioridades
+- Ensayos, clases y conciertos
+- Exámenes
+- Estadísticas
+- Exportación/importación de datos
+- Persistencia local
+- Service worker para uso offline una vez cargada
+
+Para instalarla en iPhone necesita publicarse en una URL HTTPS. El paquete está listo para subir a un servicio de hosting estático gratuito.
---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="ritmoacade-2-0";
+const ASSETS=["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let c=res.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return res}).catch(()=>caches.match("./index.html")))));
